96bdf54c017ffd185359e8bb8037a8b808405e39
gabriel-gbl <gabsribeiro.gr07@gmail.com>
2023-11-28 08:34:26 -0300

    versao para teste

---
+++ control/dados.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> INDEX </title>
+    </head>
+    <body>
+        <header> dados </html>
+    </body>
+</html>
---
+++ factory/conexao.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> INDEX </title>
+    </head>
+    <body>
+        <header> conexão </html>
+    </body>
+</html>
